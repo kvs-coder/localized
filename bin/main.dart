61c47dd5b0385ab9af26214ee5059eacd0f84452
avista1 <anton.stasevich@gmail.com>
2020-11-14 23:22:54 +0100

    Yandex Translate was added as a translation provider

--- bin/main.dart
+++ bin/main.dart
@@ -8,11 +8,28 @@ import 'package:tuple/tuple.dart';
 import 'package:translator/translator.dart';
 
 // supported translation providers
-Map<String, String> _providers = {
-  "GoogleTest": "Google API through a back door",
-  "Google": "Google using API key",
+// GoogleTest should be on the first place
+const _providerList = [
+  "GoogleTest",
+  "Google",
+  "Yandex",
+];
+
+final Map<String, Function> _providerTranslateFunctionMap = {
+  _providerList[0]: _translateGoogleTest,
+  _providerList[1]: _translateGoogle,
+  _providerList[2]: _translateYandex,
 };
 
+final Map<String, String> _providerDescriptionMap = {
+  _providerList[0]: "Google API through a back door",
+  _providerList[1]: "Google using API key",
+  _providerList[2]: "Yandex using folder ID and IAM token",
+};
+
+/// Creates examples of localized files as well as localizes strings
+/// by translating them using different providers.
+/// See README.md and usage for details
 void main(List<String> args) {
   final parser = ArgParser(allowTrailingOptions: true);
   parser.addFlag('create',
@@ -31,12 +48,16 @@ void main(List<String> args) {
   parser.addOption('provider',
       abbr: 'p',
       defaultsTo: 'GoogleTest',
-      allowed: _providers.entries.map((e) => e.key).toList(),
-      allowedHelp: _providers,
+      allowed: _providerList,
+      allowedHelp: _providerDescriptionMap,
       help: 'Provider of translation API');
-  parser.addOption('key', abbr: 'k', help: 'Project API key');
+  parser.addOption('key', abbr: 'k', help: 'Google Project API key');
+  parser.addOption('folder_id', abbr: 'f', help: 'Yandex Folder ID');
+  parser.addOption('token', abbr: 'i', help: 'Yandex IAM token');
   parser.addOption('number',
-      abbr: 'n', help: 'The number of strings to translate in one request');
+      abbr: 'n',
+      defaultsTo: '1',
+      help: 'The number of strings to translate in one request');
 
   final createFiles = parser.parse(args)['create'];
   final translateFiles = parser.parse(args)['translate'];
@@ -50,25 +71,33 @@ void main(List<String> args) {
 
   final langCodes = parser.parse(args)['languageCodes'];
   final dirPath = parser.parse(args)['dirPath'];
-  final provider = parser.parse(args)['provider'];
-  final projectKey = parser.parse(args)['key'];
-  final numStringsAtOnce = int.parse(parser.parse(args)['number']);
+
+  final Map<String, String> options = Map();
+  if (parser.parse(args)['provider'] != null)
+    options['provider'] = parser.parse(args)['provider'];
+  if (parser.parse(args)['key'] != null)
+    options['key'] = parser.parse(args)['key'];
+  if (parser.parse(args)['number'] != null)
+    options['number'] = parser.parse(args)['number'];
+  if (parser.parse(args)['folder_id'] != null)
+    options['folder_id'] = parser.parse(args)['folder_id'];
+  if (parser.parse(args)['token'] != null)
+    options['token'] = parser.parse(args)['token'];
 
   // translation from English is better
   langCodes.sort((a, b) => a == 'en' ? -1 : b == 'en' ? 1 : 0);
 
   // creating examples or translating strings?
   if (createFiles) _createLocalizedFiles(langCodes, dirPath);
-  if (translateFiles)
-    _translateLocalizedFiles(
-        langCodes, dirPath, provider, projectKey, numStringsAtOnce);
+  if (translateFiles) _translateLocalizedFiles(langCodes, dirPath, options);
 }
 
 // main function to translate
-_translateLocalizedFiles(List<String> langCodes, String dirPath,
-    String provider, String projectKey, int numStringsAtOnce) async {
-  if (!_providers.containsKey(provider)) {
-    stdout.writeln('No tranclation provider is set. Exiting...');
+_translateLocalizedFiles(
+    List<String> langCodes, String dirPath, Map<String, String> options) async {
+  final provider = options['provider'];
+  if (provider == null || !_providerList.contains(provider)) {
+    stdout.writeln('No valid translation provider set. Exiting...');
     exit(0);
   }
   if (langCodes.length < 2) {
@@ -88,7 +117,7 @@ _translateLocalizedFiles(List<String> langCodes, String dirPath,
   }
 
   // loading keys and strings to langStringMap for each language
-  Map<String, Map<String, String>> langStringMap = Map();
+  final Map<String, Map<String, String>> langStringMap = Map();
   for (final lang in langCodes) {
     if (!_isSupported(lang)) {
       stdout.writeln('Language code $lang is not supported.');
@@ -101,14 +130,14 @@ _translateLocalizedFiles(List<String> langCodes, String dirPath,
   // we need such a strange structure to translate a bunch of strings at once,
   // because this is much quicker
   // Map of <targetLang, sourceLang> to List<key>>
-  Map<Tuple2<String, String>, List<String>> toTranslateMap = Map();
+  final Map<Tuple2<String, String>, List<String>> toTranslateMap = Map();
   langStringMap.forEach((sourceLang, sourceStringMap) {
     sourceStringMap.forEach((sourceKey, sourceString) {
       langStringMap.forEach((targetLang, targetStringMap) {
         if (sourceLang != targetLang) {
           if (sourceStringMap[sourceKey].isNotEmpty &&
               !targetStringMap.containsKey(sourceKey)) {
-            var tuple = Tuple2(targetLang, sourceLang);
+            final tuple = Tuple2(targetLang, sourceLang);
             if (toTranslateMap[tuple] == null) toTranslateMap[tuple] = List();
             toTranslateMap[tuple].add(sourceKey);
           }
@@ -117,7 +146,7 @@ _translateLocalizedFiles(List<String> langCodes, String dirPath,
     });
   });
   if (toTranslateMap.isEmpty) {
-    stdout.writeln('No string needs to be translated. Exiting...');
+    stdout.writeln('No strings need to be translated. Exiting...');
     exit(0);
   }
 
@@ -126,24 +155,23 @@ _translateLocalizedFiles(List<String> langCodes, String dirPath,
   stdout.writeln('Translation starts - $date');
 
   // different functions and arguments for different providers
-  if (provider.compareTo('GoogleTest') == 0) {
-    await _translateGoogleTest(langStringMap, toTranslateMap);
-  } else if (provider.compareTo('Google') == 0) {
-    await _translateGoogle(
-        langStringMap, toTranslateMap, projectKey, numStringsAtOnce);
-  }
+  if (provider.compareTo(_providerList[0]) == 0)
+    await _providerTranslateFunctionMap[provider](
+        langStringMap, toTranslateMap);
+  else
+    await _batchTranslate(langStringMap, toTranslateMap, options);
 
   // to check performance
   date = DateTime.now().toString();
-  stdout.writeln('Translation ends - $date');
+  stdout.writeln('Translation ends   - $date');
   _updateContent(langStringMap, dirPath);
 }
 
 // see https://github.com/gabrielpacheco23, thanks to Gabriel Pacheco
-_translateGoogleTest(Map<String, Map<String, String>> langStringMap,
+Future<void> _translateGoogleTest(
+    Map<String, Map<String, String>> langStringMap,
     Map<Tuple2<String, String>, List<String>> toTranslateMap) async {
   final gtr = GoogleTranslator();
-
   await Future.forEach(toTranslateMap.entries, (toTranslate) async {
     final targetLang = toTranslate.key.item1;
     final sourceLang = toTranslate.key.item2;
@@ -157,26 +185,26 @@ _translateGoogleTest(Map<String, Map<String, String>> langStringMap,
   });
 }
 
-// Google Translate
 // langStringMap is a map of a language to <key, string>
 // toTranslateMap is a map of <targetLang, sourceLang> to List<key>> - strings to translate
-_translateGoogle(
+Future<void> _batchTranslate(
     Map<String, Map<String, String>> langStringMap,
     Map<Tuple2<String, String>, List<String>> toTranslateMap,
-    String projectKey,
-    int numStringsAtOnce) async {
+    Map<String, String> options) async {
+  final numStringsAtOnce = int.parse(options['number']);
+  String provider = options['provider'];
   await Future.forEach(toTranslateMap.entries, (toTranslate) async {
     final targetLang = toTranslate.key.item1;
     final sourceLang = toTranslate.key.item2;
     final keyList = toTranslate.value;
-    List<String> stringInOutList = List();
+    final List<String> stringInOutList = List();
     int num = 0;
     for (var key1 in keyList) {
       stringInOutList.add(langStringMap[sourceLang][key1]);
       ++num;
       if (num > 0 && num % numStringsAtOnce == 0) {
-        await _translateGoogleString(
-            stringInOutList, sourceLang, targetLang, projectKey);
+        await _providerTranslateFunctionMap[provider](
+            stringInOutList, sourceLang, targetLang, options);
         for (int index = 0; index < stringInOutList.length; index++) {
           langStringMap[targetLang]
                   [keyList[num - stringInOutList.length + index]] =
@@ -186,8 +214,8 @@ _translateGoogle(
       }
     }
     if (stringInOutList.isNotEmpty) {
-      await _translateGoogleString(
-          stringInOutList, sourceLang, targetLang, projectKey);
+      await _providerTranslateFunctionMap[provider](
+          stringInOutList, sourceLang, targetLang, options);
       for (int index = 0; index < stringInOutList.length; index++) {
         langStringMap[targetLang]
                 [keyList[num - stringInOutList.length + index]] =
@@ -197,55 +225,125 @@ _translateGoogle(
   });
 }
 
-// This is where the magic of Google Translate happens
-_translateGoogleString(List<String> stringInOutList, String sourceLang,
-    String targetLang, String projectKey) async {
-  // https://translation.googleapis.com/language/translate/v2?target={YOUR_LANGUAGE}&key=${API_KEY}&q=${TEXT}
+// Google Translate
+// https://cloud.google.com/translate/docs/basic/translating-text
+// langStringMap is a map of a language to <key, string>
+// toTranslateMap is a map of <targetLang, sourceLang> to List<key>> - strings to translate
+// https://translation.googleapis.com/language/translate/v2?target={YOUR_LANGUAGE}&key=${API_KEY}&q=${TEXT}
+Future<void> _translateGoogle(List<String> stringInOutList, String sourceLang,
+    String targetLang, Map<String, String> options) async {
+  final googleProjectKey = options['key'];
+  if (googleProjectKey == null) {
+    stdout.writeln('No Google project key provided. Exiting...');
+    exit(0);
+  }
+
   const baseUrl = 'translation.googleapis.com';
   const path = 'language/translate/v2';
   // It's impossible to add several equal query parameters using the 'parameters' map
   final queryStart =
-      'format=text&target=$targetLang&source=$sourceLang&key=$projectKey';
+      'format=text&target=$targetLang&source=$sourceLang&key=$googleProjectKey';
   var queryEnd = '';
   stringInOutList.forEach((text) {
     queryEnd = queryEnd + '&q=$text';
   });
-  final url = Uri.https(baseUrl, path).replace(query: queryStart + queryEnd);
-  final data = await http.get(url);
-  if (data.statusCode != 200) {
-    throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+  try {
+    final url = Uri.https(baseUrl, path).replace(query: queryStart + queryEnd);
+    final data = await http.get(url);
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+    }
+    stringInOutList.clear();
+    final mapList = jsonDecode(data.body)["data"]["translations"];
+    mapList.forEach((map) {
+      stringInOutList.add(map['translatedText']);
+    });
+  } catch (e) {
+    stdout.writeln(
+        'Cannot translate some strings from $sourceLang to $targetLang. An excaption occurs:\n$e');
+    stringInOutList.clear();
   }
-  stringInOutList.clear();
-  final mapList = jsonDecode(data.body)["data"]["translations"];
-  mapList.forEach((map) {
-    stringInOutList.add(map['translatedText']);
-  });
+}
+
+// Yandex Translate
+// https://cloud.yandex.com/docs/iam/operations/iam-token/create
+// langStringMap is a map of a language to <key, string>
+// toTranslateMap is a map of <targetLang, sourceLang> to List<key>> - strings to translate
+Future<void> _translateYandex(List<String> stringInOutList, String sourceLang,
+    String targetLang, Map<String, String> options) async {
+  final yandexFilderID = options['folder_id'];
+  final yandexIAMToken = options['token'];
+  if (yandexFilderID == null || yandexIAMToken == null) {
+    stdout.writeln('No Yandex Folder ID or IAM token provided. Exiting...');
+    exit(0);
+  }
+
+  const url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
+
+  final Map<String, String> headers = {
+    'Content-type': 'application/json',
+    'Authorization': "Bearer " + yandexIAMToken,
+  };
+  try {
+    final body = json.encode({
+      "sourceLanguageCode": sourceLang,
+      "format": "PLAIN_TEXT",
+      "folder_id": yandexFilderID,
+      "texts": stringInOutList,
+      "targetLanguageCode": targetLang
+    });
+
+    final data = await http.post(url, body: body, headers: headers);
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}');
+    }
+
+    stringInOutList.clear();
+    final mapList = jsonDecode(data.body)["translations"];
+    mapList.forEach((map) {
+      stringInOutList.add(map['text']);
+    });
+  } catch (e) {
+    stdout.writeln(
+        'Cannot translate some strings from $sourceLang to $targetLang. An excaption occurs:\n$e');
+    stringInOutList.clear();
+  }
 }
 
 // Loading of strings from language files
 Future<Map<String, String>> _loadStrings(String lang, String dirPath) async {
   Map<String, String> localizedStrings = Map();
-  File file = File('$dirPath/$lang.json');
-  if (!await file.exists()) return localizedStrings;
-  String jsonString = await file.readAsString();
-  if (jsonString.isEmpty) return localizedStrings;
-  Map<String, dynamic> jsonMap = json.decode(jsonString);
-  localizedStrings = jsonMap.map((key, value) {
-    return MapEntry(key, value.toString());
-  });
+  try {
+    final file = File('$dirPath/$lang.json');
+    if (!await file.exists()) return localizedStrings;
+    final jsonString = await file.readAsString();
+    if (jsonString.isEmpty) return localizedStrings;
+    final Map<String, dynamic> jsonMap = json.decode(jsonString);
+    localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+  } catch (e) {
+    stdout.writeln(
+        'Cannot load strings from $dirPath/$lang.json file. An excaption occurs:\n$e.');
+  }
+
   return localizedStrings;
 }
 
 // Writing translated files
-_updateContent(Map<String, Map<String, String>> langStrMap, dirPath) {
+void _updateContent(Map<String, Map<String, String>> langStrMap, dirPath) {
   Future.forEach(langStrMap.entries, (langStrMapEntry) async {
-    String jsonStr = json.encode(langStrMapEntry.value);
-    await File('$dirPath/${langStrMapEntry.key}.json')
-        .create(recursive: true)
-        .then((file) {
-      stdout.writeln('Rewriting file: ${file.path}');
-      file.writeAsString(jsonStr);
-    });
+    try {
+      await File('$dirPath/${langStrMapEntry.key}.json')
+          .create(recursive: true)
+          .then((file) {
+        stdout.writeln('Rewriting file: ${file.path}');
+        file.writeAsString(json.encode(langStrMapEntry.value));
+      });
+    } catch (e) {
+      stdout.writeln(
+          'Cannot update $dirPath/${langStrMapEntry.key}.json file. An excaption occurs:\n$e.');
+    }
   });
 }
 
